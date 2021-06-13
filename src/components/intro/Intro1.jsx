c1689f586261624ff739cc185218159550daba3a
Shivu02062002 <75877929+Shivu02062002@users.noreply.github.com>
2021-06-13 10:56:25 +0530

    added carousel to the intro page

--- src/components/intro/Intro1.jsx
+++ src/components/intro/Intro1.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import code from "./code.png";
+import laptop from "./laptop.jpg";
 
 function Intro1() {
     return (
         <div>
             <h1 className="intro-heading">HI CODERS!!!</h1>
+            <Carousel>
+                <Carousel.Item className="carousel-design" interval={1000}>
+                    <h2>
+                        Welcome to the World of Codes!!!
+                    </h2>
+                    <div>
+                        <img src={code} alt="code" width="500" height="500" />
+                        <img src={laptop} alt="laptop" width="500" height="500" />
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-design" interval={1000}>
+                    <h2>
+                        Vast Variety of resources to satisfy your hunger for knowledge.
+                    </h2>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
--- src/components/intro/IntroFinal.jsx
+++ src/components/intro/IntroFinal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Intro1 from "./Intro1";
+import FooterText from "../Top&Bottom/Footer";
 
 function IntroFinal() {
     return (
         <div>
             <Intro1 />
+            <FooterText />
         </div>
     )
 }
